f8032e28a9c292d964dfc11fdc10404508b029ea
Martin Schmidt <martin@scoding.dk>
2021-11-29 13:54:52 +0000

    removed input managed identity

--- infrastructure/azure/az_aks_managed_identity.tf
+++ infrastructure/azure/az_aks_managed_identity.tf
@@ -3,7 +3,6 @@ resource "azurerm_user_assigned_identity" "aks_mid" {
     azurerm_resource_group.main,
   ]
 
-  count               = var.azure_aks_spn_create ? 1 : 0
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   name                = var.azure_aks_spn_name
@@ -15,10 +14,4 @@ resource "azurerm_user_assigned_identity" "aks_mid" {
       tags,
     ]
   }
-}
-
-data "azurerm_user_assigned_identity" "aks_mid" {
-  count               = var.azure_aks_spn_create ? 0 : 1
-  name                = var.azure_aks_spn_name
-  resource_group_name = var.azure_aks_spn_rg
 }
